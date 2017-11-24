1690cfb1493f8118600d55a43a23cbbeae0c872e
John Rodney <johnrodney84@gmail.com>
2017-11-23 18:23:36 -0600

    refactor utility functions

--- dist/src/game/utilities/utility-functions.js
+++ dist/src/game/utilities/utility-functions.js
@@ -10,17 +10,17 @@ function offscreen(dot) {
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
+  var name = cname + '=';
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
   for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) === ' ') {
       c = c.substring(1);
     }
-    if (c.indexOf(name) == 0) {
+    if (c.indexOf(name) === 0) {
       return c.substring(name.length, c.length);
     }
   }
-  return "";
+  return '';
 }
